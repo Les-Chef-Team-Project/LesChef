c0f39decd763b4f6cd6dbe0e13b1cca2505842a8
Yoon-jae-min <jmyoon1994@naver.com>
2024-05-20 14:52:53 +0900

    start

---
+++ LesChefBoot/LesChef/src/main/java/com/example/leschef/LesChefApplication.java
@@ -0,0 +1,11 @@
+package com.example.leschef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesChefApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LesChefApplication.class, args);
+    }
+}
