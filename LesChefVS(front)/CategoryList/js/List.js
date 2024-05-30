0584992132c443bd8f96457be4f8ee1ebec53fb9
stagechaplin <https://github.com/stagechaplin/stagechaplin.git>
2024-05-30 18:02:49 +0900

    0530

---
+++ LesChefVS(front)/CategoryList/js/List.js
@@ -0,0 +1,39 @@
+// 공유 카테고리 선택 시 사용자 아이디 및 이미지 부여 
+
+const nickname = document.querySelectorAll('.nickname');
+const nickname2 = document.querySelectorAll('.icon');
+const sharepage = document.querySelectorAll('.categorybox');
+
+sharepage.forEach((share, index) => {
+  share.addEventListener("click", function(){
+    if(index == 4){
+    nickname.forEach(function(name){
+      name.style.display = "block";
+    });
+    nickname2.forEach(function(name2){
+      name2.style.display = "block";  
+    });
+  }else{
+    nickname.forEach(function(name){
+      name.style.display = "none";
+    });
+    nickname2.forEach(function(name2){
+      name2.style.display = "none";  
+    });
+    }
+  })
+})
+
+// 세부 카테고리 컬러 바꾸기
+const categoryname = document.querySelectorAll('.subcategory');
+categoryname.forEach((name, index) =>{
+  name.addEventListener('click',()=>{
+    categoryname.forEach((name2, index2) => {
+      if(index == index2){
+        name2.style.color = 'black';
+      }else{
+        name2.style.color = '#616161';
+      }
+    })
+  })
+})
