53f1e309bde3d55deb32acc3538046540d548f8b
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-06-13 00:31:08 +0900

    20240613-LecipeLoginUI-Update-jm

--- UI 통합본/js/List.js
+++ UI 통합본/js/List.js
@@ -70,16 +70,26 @@ categoryname.forEach((name, index) =>{
 const loginModal = document.querySelector('.loginModal');
 const loginBox = document.querySelector('.Login-box');
 const loginButton = document.getElementById('login');
-
+const goToFindButton = document.querySelector('#FindIdPw');
+const loginInputForm = document.querySelector('.login-form-container');
+const findInputForm = document.querySelector('.find-form-container');
+const findIdForm = document.querySelector('.id-form-container');
+const findPwForm = document.querySelector('.pw-form-container');
+const findIdButton = document.querySelector('.id-button');
+const findPwButton = document.querySelector('.pw-button');
+const findToLogin = document.querySelector('.find-to-login span');
+const findToLoginText = document.querySelector('.find-to-login');
 
 function loginModalActive() {
+  loginInputForm.style.display = "block";
+  findInputForm.style.display = "none";
+  findToLoginText.style.display = "none";
   if(loginModal.classList.contains('active')){
+    loginBox.style.transition = "none";
     loginBox.classList.remove('active');
-    setTimeout(() => {
-      loginModal.classList.remove('active');
-    }, 500);
-    
+    loginModal.classList.remove('active');
   }else{
+    loginBox.style.transition = "all 0.5s";
     loginBox.querySelector("input").value = "";
     loginModal.classList.add('active');
     loginBox.classList.add('active');
@@ -92,3 +102,33 @@ loginBox.addEventListener('click', function(event) {
 
 loginButton.addEventListener('click', loginModalActive);
 loginModal.addEventListener('click', loginModalActive);
+
+goToFindButton.addEventListener('click', () => {
+  loginInputForm.style.display = "none";
+  findInputForm.style.display = "block";
+  findIdForm.style.display = "block";
+  findPwForm.style.display = "none";
+  findIdButton.style.backgroundColor = "#79AE84";
+  findPwButton.style.backgroundColor = "#28442d";
+  findToLoginText.style.display = "block";
+});
+
+findIdButton.addEventListener('click',() => {
+  findIdButton.style.backgroundColor = "#79AE84";
+  findPwButton.style.backgroundColor = "#28442d";
+  findIdForm.style.display = "block";
+  findPwForm.style.display = "none";
+});
+
+findPwButton.addEventListener('click', () => {
+  findIdButton.style.backgroundColor = "#28442d";
+  findPwButton.style.backgroundColor = "#79AE84";
+  findIdForm.style.display = "none";
+  findPwForm.style.display = "block";
+});
+
+findToLogin.addEventListener('click', () => {
+  loginInputForm.style.display = "block";
+  findInputForm.style.display = "none";
+  findToLoginText.style.display = "none";
+});
